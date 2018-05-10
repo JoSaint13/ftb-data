62abb4390a9c917474c1893539300f9f14de8734
ZherditskiyAndrey <az@nustay.com>
2018-05-11 00:49:18 +0300

    init

--- ng-ftb-data/ClientApp/app/app.shared.module.ts
+++ ng-ftb-data/ClientApp/app/app.shared.module.ts
@@ -4,30 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './components/app/app.component';
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
-import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent
+        AppComponent
     ],
     imports: [
         CommonModule,
         HttpModule,
         FormsModule,
         RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
+            //{ path: '', redirectTo: 'home', pathMatch: 'full' },
+            //{ path: '**', redirectTo: 'home' }
         ])
     ]
 })
